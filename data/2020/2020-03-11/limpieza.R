00a7a530e410b486177b37d8ec509c92c1a3c67b
arturochian <arturob.chian@gmail.com>
2020-03-12 12:35:16 -0500

    actualizando nombres

--- data/2020/2020-03-11/limpieza.R
+++ data/2020/2020-03-11/limpieza.R
@@ -4,6 +4,6 @@ library(ggplot2)
 library(RColorBrewer)
 
 setwd("D:/ABCN/Github/manos-a-la-data/data/2020/2020-03-11")
-encuesta<-readxl::read_xlsx("c_15.xlsx",sheet = "final")
-write.csv(encuesta,"encuesta.csv",row.names = F)
+encuesta<-readxl::read_xlsx("encuesta.xlsx",sheet = "final")
+write.csv(encuesta,"encuesta2.csv",row.names = F,fileEncoding = "UTF-8")
 
