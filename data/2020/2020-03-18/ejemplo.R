760c13fcd7c389f4b744be36da3dd1907d71f7c5
arturochian <arturob.chian@gmail.com>
2020-03-18 07:49:16 -0500

    actualizando semana 3

--- data/2020/2020-03-18/ejemplo.R
+++ data/2020/2020-03-18/ejemplo.R
@@ -1,14 +1,9 @@
-library(stringi)
 library(dplyr)
 library(ggplot2)
-library(RColorBrewer)
-library(forecast)
-library(xts)
 
 setwd("D:/ABCN/Github/manos-a-la-data/data/2020/2020-03-18")
-covid19peru<-read.csv("covid19peru.csv")
+covid19peru<-read.csv("https://raw.githubusercontent.com/BESTDATASCIENCE/manos-a-la-data/master/data/2020/2020-03-18/covid19peru.csv")
 covid19peru$fecha <- as.Date(covid19peru$fecha)
-covid19<-xts(x=covid19peru$contagiados,order.by =covid19peru$fecha)
 
 p <- ggplot(covid19peru, aes(x=fecha, y=contagiados,group = 1)) +
   geom_line() + geom_smooth(method = "gam")+
