e9cae46d9adc4b6aacaa3d2a98a17f130a730122
arturochian <arturob.chian@gmail.com>
2021-03-03 22:16:32 -0500

    week 9 - 2021

---
+++ data/2021/2021-03-03/ejemplo.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(dplyr)
+library(vroom)
+library(readstata13)
+library(foreign)
+library(here)
+options(scipen=999)
+
+library(XML)
+library(RCurl)
+library(rlist)
+library(htmltab)
+
+
+setwd(here::here("data/2021/2021-03-03/"))
+peajes1<- readxl::read_xlsx("202007-PDE-REP-00029_0.xlsx")
+peajes2<- readxl::read_xlsx("202007-PDE-REP-00026_0.xlsx")
